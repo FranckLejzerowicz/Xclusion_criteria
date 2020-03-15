059a012f10ad9dfd5394292e4613530ecaae4f6e
FranckLejzerowicz <franck.lejzerowicz@unige.ch>
2020-03-15 11:17:40 -0700

    solved criteria format change -> code change

--- Xclusion_criteria/_xclusion_dtypes.py
+++ Xclusion_criteria/_xclusion_dtypes.py
@@ -184,7 +184,6 @@ def split_variables_types(dtypes: dict, criteria: dict,
     categorical : list
         Metadata variables that are categorical.
     """
-    print(criteria)
     all_criteria_variables = set(
         [y[0] for x in ['init', 'add', 'filter'] for y in criteria[x].keys() if x in criteria]
     )
